cbf254b23eb663addbf577367f9d3c1bbe5db349
Razielly <nongpaed@gmail.com>
2025-08-15 12:25:02 +0700

    v.3:Test5

--- test5.py
+++ test5.py
@@ -1,1 +1,29 @@
-#jjj
+# input 
+Name =  input('Enter name: ')
+year_born =  input('YYYY: ')
+
+# Variable คือชื่อที่เดฟตั้งขึ้นมาเองเอาไว้เก็บข้อมูล หรือการกำหนดตัวแปร
+# 
+
+print('--------------')
+print(f'Hi {Name} your BY is {year_born}')
+print(f'Your old is {2025 -int(year_born)}')
+print('--------------')
+# ใช้ ,
+print('Hi',Name,'your BY is',year_born)
+print('your old is',(2025 - int(year_born)))
+print('--------------')
+
+
+
+# ใช้ +
+print('Hi'+' '+Name +'your'+' '+"BY"+" "+'is'+' '+year_born)
+print('Your'+' '+'old'+' '+'is'+' '+str(2025 - int(year_born)))
+print('--------------')
+
+
+
+# ใช้ format
+print('Hi {} your BY is {}'.format(Name,year_born))
+print('Your old is {}'.format(2025 - int(year_born)))
+print('--------------')
