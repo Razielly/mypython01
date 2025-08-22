66f4b137bd414ba996d30f62f98fa877c68528b5
IValentineI <S6519410019@sau.ac.th>
2025-08-22 12:25:01 +0700

    test9,10.sucest

---
+++ test10.py
@@ -0,0 +1,8 @@
+print("----------------------------")
+print("โปรแกรมการคำนวณค่าเฉลี่ยของผลรวม")
+print("----------------------------")
+num1=float(input("เลขตัวที่1: ")); num2=float(input("เลขตัวที่2: ")); num3=float(input("เลขตัวที่3: ")); num4=float(input("เลขตัวที่4: ")); num5=float(input("เลขตัวที่5:"))
+sum = num1+num2+num3+num4+num5
+sum_1 = sum  / 5
+print(f'ผลรวมของเลข5จำนวน = {sum}')
+print(f'ผลเฉลี่ย = {sum_1}')
